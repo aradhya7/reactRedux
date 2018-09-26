b63c89db3ab033bfa7612d01291633f18b120315
aradhya7 <karthikaradhya.007@gmail.com>
2018-09-26 09:33:10 +0530

    basics

---
+++ reactComponentLifecyles.js
@@ -0,0 +1,15 @@
+// Mounting
+// constructor()
+// static getDerivedStateFromProps()
+// render()
+// componentDidMount()
+// 
+// Updating
+// static getDerivedStateFromProps()
+// shouldComponentUpdate()
+// render()
+// getSnapshotBeforeUpdate()
+// componentDidUpdate()
+// 
+// Unmounting
+// componentWillUnmount()
---
+++ reactRouter.js
@@ -0,0 +1,5 @@
+//BrowserRouter
+//Route
+//Link
+//match
+//
